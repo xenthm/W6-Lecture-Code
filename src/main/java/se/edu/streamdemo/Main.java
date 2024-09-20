17f3135242718c2cf14f2b9b369b531c4f8b3e5b
xenthm <56202777+xenthm@users.noreply.github.com>
2024-09-20 17:37:45 +0800

    Use lambdas to filter tasks and sort them

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -6,20 +6,25 @@ import se.edu.streamdemo.task.Task;
 
 import java.util.ArrayList;
 
+import static java.util.stream.Collectors.toList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Task manager (using streams)");
         Datamanager dataManager = new Datamanager("./data/data.txt");
         ArrayList<Task> tasksData = dataManager.loadData();
 
-        System.out.println("Printing all data ...");
-        printAllData(tasksData);
+//        System.out.println("Printing all data ...");
+//        printAllData(tasksData);
 
         System.out.println("Printing deadlines ...");
         printDeadlines(tasksData);
+        printSortedDeadlinesWithStreams(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+        ArrayList<Task> filteredList = filterTasksByString(tasksData, "11");
+        printAllData(filteredList);
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
@@ -46,4 +51,21 @@ public class Main {
         }
     }
 
+    public static void printSortedDeadlinesWithStreams(ArrayList<Task> tasksData) {
+        System.out.println("Printing deadlines with streams");
+        tasksData.stream()                                                          // create stream
+                .filter(t -> t instanceof Deadline)                                 // filter deadlines
+                .sorted((t1, t2) ->                                                 // sorts with comparator compareToIgnoreCase
+                    t1.getDescription().compareToIgnoreCase(t2.getDescription())
+                )
+                .forEach(System.out::println);                                      // print each
+    }
+
+    public static ArrayList<Task> filterTasksByString(ArrayList<Task> tasksData, String filterString) {
+        return (ArrayList<Task>) tasksData.stream()             // create stream
+                .filter(t ->                                    // filter tasks with filterString
+                    t.getDescription().contains(filterString)
+                )
+                .collect(toList());                             // collect List and cast to ArrayList<Task>
+    }
 }
